188e1fe2d4635ec1d48ff3fd48bbb283bf92a3ed
Dirly <derek.kaleida@gmail.com>
2015-03-28 19:52:25 -0400

    scafolding project

--- build/app.js
+++ build/app.js
@@ -1,12 +1,89 @@
 (function(){
-    var app = angular.module('portfolio',[ ]);
-    
-    app.controller('PortfolioCtrl', function(){
-    	this.windowHeight = docHeight;
-    });
+	var app = angular.module('port',[ ]);
 
-    var docHeight = "innerHeight" in window 
-    				? window.innerHeight
-    				: document.documentElement.offsetHeight; 
+//POPUP
+	app.directive('popup', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'views/popup.html',
+			controller: function(){
+
+			},
+			controllerAs:'popup'
+		};
+	});
+
+//MENU
+	app.directive('menu', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'views/menu.html',
+			controller: function(){
+
+			},
+			controllerAs:'menu'
+		};
+	});
+
+//TOPBAR
+	app.directive('top-bar', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'views/topBar.html',
+			controller: function(){
+
+			},
+			controllerAs:'topBar'
+		};
+	});
+
+//HOME
+	app.directive('home', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'views/home.html',
+			controller: function(){
+
+			},
+			controllerAs:'home'
+		};
+	});
+
+//ABOUT
+	app.directive('about', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'views/about.html',
+			controller: function(){
+
+			},
+			controllerAs:'about'
+		};
+	});
+
+//PORTFOLIO
+	app.directive('portfolio', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'views/portfolio.html',
+			controller: function(){
+
+			},
+			controllerAs:'portfolio'
+		};
+	});
+
+
+//CONTACT
+	app.directive('contact', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'views/contact.html',
+			controller: function(){
+
+			},
+			controllerAs:'contact'
+		};
+	});
 
 })();
